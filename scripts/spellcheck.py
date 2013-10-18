fb8c2cf1c5ed8d22d693743583cc1bc39d95344e
dwgill <daniel.gill@my.wheaton.edu>
2013-10-17 19:43:14 -0500

    Added memoize, reimplemented spellcheck.

--- scripts/spellcheck.py
+++ scripts/spellcheck.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 '''
-Created on Oct 11, 2013
+Crated on Oct 15, 2013
 
 @author: tvandrun
 '''
@@ -8,21 +8,31 @@ Created on Oct 11, 2013
 import nltk
 from nltk.corpus import PlaintextCorpusReader
 import sys
+from nltk.model import NgramModel
+from memoize import memoize
 
-def correct_spell(word):
+def _main():
+    print(correct_file(sys.argv[1]))
+
+def correct_file(file_path):
+    with open(file_path) as opened_file:
+        return correct_lines(opened_file)
+
+def correct_lines(lines):
+    corrected_lines = []
+    for line in lines:
+        tokens_raw = nltk.word_tokenize(line)
+        corrected_line = ''
+        for word in tokens_raw:
+            if word.isalpha():
+                corrected_line += correct_spelling(word) + ' '
+            else:
+                corrected_line += word + ' '
+        corrected_lines.append(corrected_line)
+    return corrected_lines
+
+def correct_spelling(word):
     return word
 
-source_file = open(sys.argv[1], 'r')
-
-for line in source_file :
-    tokens_raw = nltk.word_tokenize(line)
-    
-    corrected_line = ''
-    for word in tokens_raw :
-        if word.isalpha() :
-            corrected_line += correct_spell(word) + ' '
-        else :
-            corrected_line += word + ' '
-    print corrected_line
-    
-source_file.close()
+if __name__ == "__main__":
+    _main()
