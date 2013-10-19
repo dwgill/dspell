1c590c8303b0d26ed664492c276e494378089662
dwgill <daniel.gill@my.wheaton.edu>
2013-10-19 02:30:28 -0500

    Added some elaborative comments to experiment.py

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -10,11 +10,22 @@ import nltk
 from nltk.corpus import PlaintextCorpusReader
 
 def edit_distance(source, target, scores=[2,3,2,3,0], cut_off=sys.maxint):
+
+    # Make a zeroed out matrix of len(target)+1 by len(source)+1.
     distances = [[0 for j in range(len(source) + 1)] for i in range(len(target) + 1)]
+
+    # Make all values distances[0..len(target)+1][0] = range(len(target) + 1)
+    # i.e. for any given target, it will take i insertions to turn an empty string into that target.
     for i in range(len(target) + 1) :
         distances[i][0] = scores[0] * i
+
+    # Make all values distances[0][0..len(source)] = range(len(source))
+    # i.e. for any given empty string
     for j in range(len(source) + 1) :
         distances[0][j] = scores[1] * j
+
+
+
     for i in range(1, len(target) + 1) :
         for j in range(1, len(source) + 1) :
             candidate_edits = [distances[i-1][j]+scores[0],  # insert
